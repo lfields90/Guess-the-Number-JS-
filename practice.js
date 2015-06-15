f61d734f777c4160c2b6c6fc51688b7ab0d34680
Twelvvy <lamonte.fields@gmail.com>
2015-06-15 11:48:48 -0400

    Practice done, moving on to challenge

---
+++ practice.js
@@ -0,0 +1,18 @@
+alert("does this work?");
+alert("You've won an iPad");
+
+var name = prompt('What is your name?');
+alert("Your name is " + name);
+
+var wow = prompt('How much wow?');
+
+var spammer = prompt("Type in a name to find out if they are a spammer or not");
+if (spammer.toLowerCase() === 'danelson') {
+  alert(spammer + ' is definitely a spammer');
+} else {
+  alert(spammer + ' is not a spammer');
+}
+
+for (var i =0; i < 10; i++) {
+  alert ("You've won an iPad!");
+}
